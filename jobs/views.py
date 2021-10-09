977ddad20fd684ad129bfc8b827b6307f0fe89b8
yashp82099 <yash82099@yahoo.com>
2021-10-09 17:49:38 -0400

    added jobs list

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Jobs
+
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = Jobs.objects
+    return render(request, 'jobs/home.html', {'jobs':jobs})
