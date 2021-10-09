977528a84f82dbf6609979fbee712652de5a33b5
yashp82099 <yash82099@yahoo.com>
2021-10-09 00:16:59 -0400

    jobs migration done

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,11 +77,26 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD':'Baps2277!',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'portfoliodb',
+#         'USER':'portgres',
+#         'PASSWORD':'django1234',
+#         'HOST':'localhost',
+#         'PORT':'5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
