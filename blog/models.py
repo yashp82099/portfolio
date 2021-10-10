e25014092dfe02d5e013b6222f352476ba1a5cc9
yashp82099 <yash82099@yahoo.com>
2021-10-09 21:31:01 -0400

    done for now

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,14 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+
+    def __str__(self):
+        return self.title
+
+
+    def summary(self):
+        if len(self.body) > 100:
+            return self.body[:100]+'...'
+        else:
+            return self.body
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 # Create your views here.
+
+def allblogs(request):
+
+    blog = Blog.objects
+
+    return render(request, 'blog/allblogs.html', {'blog':blog})
+
+
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/blogdetail.html', {'blog':blog})
